120541b6a3d1e28222dde9d3f9359ed8a7dd1fc1
Boopathi <Boopathi@BoopathiPC>
2020-08-09 08:22:45 +0530

    Initial Commit

---
+++ TestNGParameter/src/Parameter/BothCorrect.java
@@ -0,0 +1,28 @@
+package Parameter;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+public class BothCorrect {
+	
+	@Test
+	@Parameters({"NAme","Pass"})
+	public void bothcorrect(String uname,String password)
+	{
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Boopathi\\Documents\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/");
+		
+		driver.findElement(By.id("txtUsername")).sendKeys(uname);
+		driver.findElement(By.id("txtPassword")).sendKeys(password);
+		driver.findElement(By.name("Submit")).click();
+		driver.close();
+	}
+
+
+}
---
+++ TestNGParameter/src/Parameter/CorrectUname.java
@@ -0,0 +1,26 @@
+package Parameter;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+
+public class CorrectUname {
+	@Test
+	@Parameters({"NAme","Pass"})
+	public void correctUname(String uname,String password)
+	{
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\Boopathi\\Documents\\Selenium\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://opensource-demo.orangehrmlive.com/");		
+		driver.findElement(By.id("txtUsername")).sendKeys(uname);
+		driver.findElement(By.id("txtPassword")).sendKeys(password);
+		driver.findElement(By.name("Submit")).click();
+		driver.close();
+	}
+
+
+}
